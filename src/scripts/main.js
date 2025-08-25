2b1aac2e167a74e88a27c1efbb4d23c8b3c47f9c
Andrew Shtyka <andrii.shtyka@gmail.com>
2025-08-25 21:19:45 +0300

    refactor html and js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,72 +1,101 @@
 import validator from "validator";
 
-const cardSubscribe = document.getElementById("card-subscribe");
-const cardSubscribeBackup = cardSubscribe.innerHTML;
-let userEmail = "";
+initAndStart();
 
-//////////////////////////////////////////////////
-//////////////////////////////////////////////////
-//////////////////////////////////////////////////
-//
-// init form and start saving data to local storage
-function initForm() {
-  const emailInput = document.getElementById("email-input");
+function initAndStart() {
+  storeAndWriteData();
   const form = document.getElementById("form");
-
-  emailInput.value = localStorage.getItem("email") || "";
-  emailInput.addEventListener("input", (e) => {
-    localStorage.setItem("email", e.target.value);
-  });
-
   form.addEventListener("submit", handleSubmit);
 }
-
-initForm();
-
 //////////////////////////////////////////////////
 //////////////////////////////////////////////////
 //////////////////////////////////////////////////
 //
-// submit screen logic
-function handleSubmit(e) {
-  e.preventDefault();
+// store and retrieve data from local storage
+function storeAndWriteData() {
+  const inputEmail = document.getElementById("email");
+  let storeEmail = localStorage.getItem("email") || "";
 
-  const emailInput = document.getElementById("email-input");
-  const errorMessage = document.getElementById("email-error");
-
-  if (!validator.isEmail(emailInput.value)) {
-    errorMessage.textContent = "Valid email required";
-    return;
+  if (storeEmail) {
+    inputEmail.value = storeEmail.trim();
   }
 
-  errorMessage.textContent = "";
-  localStorage.removeItem("email");
-  userEmail = emailInput.value;
-
-  cardSubscribe.innerHTML = `
-    <article>
-        <svg height="16" width="16">
-            <use href="" />
-        </svg>
-        <h2>Thanks for subscribing!</h2>
-        <p>A confirmation email has been sent to <span>${userEmail}</span>. Please open it and click the button inside to confirm your subscription.</p>
-        <button id="dismiss-button" type="button">Dismiss message</button>
-    </article>
-    `;
-
-  console.log(userEmail);
-  // add animation here
+  inputEmail.addEventListener("input", () => {
+    localStorage.setItem("email", inputEmail.value.trim());
+  });
 }
 
 //////////////////////////////////////////////////
 //////////////////////////////////////////////////
 //////////////////////////////////////////////////
 //
-// success screen logic
-cardSubscribe.addEventListener("click", (e) => {
-  if (e.target.matches("#dismiss-button")) {
-    cardSubscribe.innerHTML = cardSubscribeBackup;
-    initForm();
-    // add animation here
+// email validation (on submit)
+function validation() {
+  const inputEmail = document.getElementById("email");
+  const errorEmail = document.getElementById("error-email");
+
+  if (inputEmail.value === "") {
+    errorEmail.textContent = "Please type email";
+    return false;
+  } else if (!validator.isEmail(inputEmail.value)) {
+    errorEmail.textContent = "Valid email required";
+    return false;
+  } else {
+    errorEmail.textContent = "";
+    return true;
   }
-});
+}
+
+//////////////////////////////////////////////////
+//////////////////////////////////////////////////
+//////////////////////////////////////////////////
+//
+// submit logic
+function handleSubmit(event) {
+  event.preventDefault();
+  const isValid = validation();
+
+  if (isValid) {
+    // dev check of sent data
+    const form = document.getElementById("form");
+    const formData = new FormData(form);
+    for (const [key, value] of formData.entries()) {
+      console.log(`${key}: ${value}`);
+    }
+
+    showSuccess();
+  }
+}
+
+//////////////////////////////////////////////////
+//////////////////////////////////////////////////
+//////////////////////////////////////////////////
+//
+// success message logic
+function showSuccess() {
+  const userEmail = localStorage.getItem("email");
+  const markup = document.getElementById("card-subscribe");
+  let markupBackup = markup.innerHTML;
+
+  markup.innerHTML = `
+  <article>
+    <svg width="64" height="64">
+      <use href=""></use>
+    </svg>
+    <h2>Thanks for subscribing!</h2>
+    <p>A confirmation email has been sent to <strong>${userEmail}</strong>. Please open it and click the button inside to confirm your subscription</p>
+    <button id="button-dismiss" type="button">Dismiss message</button>
+  </article>
+  `;
+
+  markup.addEventListener("click", (event) => {
+    if (event.target.matches("#button-dismiss")) {
+      markup.innerHTML = markupBackup;
+
+      localStorage.removeItem("email");
+      const form = document.getElementById("form");
+      form.reset();
+      initAndStart();
+    }
+  });
+}
