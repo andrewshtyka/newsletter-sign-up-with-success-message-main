6a0b27606a6a899e971ed5ff1c1cb70e52ccfc16
Andrew Shtyka <andrii.shtyka@gmail.com>
2025-08-25 21:53:38 +0300

    refactor js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,6 +5,7 @@ initAndStart();
 function initAndStart() {
   storeAndWriteData();
   const form = document.getElementById("form");
+  form.removeEventListener("submit", handleSubmit);
   form.addEventListener("submit", handleSubmit);
 }
 //////////////////////////////////////////////////
@@ -78,19 +79,24 @@ function showSuccess() {
   let markupBackup = markup.innerHTML;
 
   markup.innerHTML = `
-  <article>
-    <svg width="64" height="64">
-      <use href=""></use>
-    </svg>
-    <h2>Thanks for subscribing!</h2>
-    <p>A confirmation email has been sent to <strong>${userEmail}</strong>. Please open it and click the button inside to confirm your subscription</p>
-    <button id="button-dismiss" type="button">Dismiss message</button>
-  </article>
+    <article>
+      <svg width="64" height="64">
+        <use href=""></use>
+      </svg>
+      <h2>Thanks for subscribing!</h2>
+      <p>A confirmation email has been sent to <strong>${userEmail}</strong>. Please open it and click the button inside to confirm your subscription</p>
+      <button id="button-dismiss" type="button">Dismiss message</button>
+    </article>
   `;
 
-  markup.addEventListener("click", (event) => {
+  // clone of container (to remove old listeners)
+  const freshMarkup = markup.cloneNode(true);
+  markup.parentNode.replaceChild(freshMarkup, markup);
+
+  // listener on btn dismiss
+  freshMarkup.addEventListener("click", (event) => {
     if (event.target.matches("#button-dismiss")) {
-      markup.innerHTML = markupBackup;
+      freshMarkup.innerHTML = markupBackup;
 
       localStorage.removeItem("email");
       const form = document.getElementById("form");
